82729389171088c17d41aa4c3342339f0f92d5b8
yuexiriguang <482819895@qq.com>
2017-12-20 17:39:07 +0800

    debug

--- kafka_to_es/install.sh
+++ kafka_to_es/install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+function init_log() {
+    LOG_DIR="$(pwd)/install_log"
+
+    if [ -z $(ls | sed -n '/install_log/p') ]
+    then
+        mkdir $LOG_DIR
+    fi
+
+	INSTALL_LOG="$LOG_DIR/$(date '+%G-%m-%d_%H:%M:%S').log"
+}
+
 function log() {
 	if [ -z $2 ]
 	then
-		echo "$(date '+%G-%m-%d %H:%M:%S'): INF   detail: $1"  >> $LOG
+		echo "$(date '+%G-%m-%d %H:%M:%S'): INF   detail: $1"  >> $INSTALL_LOG
 	else
-		echo "$(date '+%G-%m-%d %H:%M:%S'): CRT   detail: $1" >> $LOG 
+		echo "$(date '+%G-%m-%d %H:%M:%S'): CRT   detail: $1" >> $INSTALL_LOG 
 		exit 1
 	fi
 }
@@ -22,7 +33,6 @@ function env_var() {
 }
 
 function init_vars() {
-	LOG="$(pwd)/install-$(date '+%G-%m-%d_%H:%M:%S').log"
 	APT_HOME=$(env_var "${APT_HOME}" "get env_var APT_HOME")
 	KAFKA_TO_ES_PKG="$APT_HOME/package/kafka_to_es"
 	CONF_FILE="$KAFKA_TO_ES_PKG/conf/conf.ini"
@@ -46,10 +56,13 @@ function mv_install_pkg() {
 
 function conf_file() {
 	partitionSet=0
-        #kafka_brokers=$(apt_config_show kafka brokers)
-	kafka_brokers=192.168.1.103,192.168.1.102
+    #kafka_brokers=$(apt_config_show kafka brokers)
+	$kafka_brokers=192.168.1.11;192.168.1.12;192.168.1.13
+	kafka_brokers=$(echo $kafka_brokers | sed 's/;/,/g')
 	
-	es_nodes=192.168.1.103,192.168.1.102
+	#es_nodes=$(apt_config_show es nodes)
+	es_nodes=10.88.1.102;10.88.1.103
+	es_nodes=$(echo es_nodes | sed 's/;/,/g')
 	es_port=9200
 	
 	echo " " > $CONF_FILE
@@ -68,6 +81,7 @@ nodes = '$es_nodes' \
 port = '$es_port'' $CONF_FILE	
 }
 
+init_log
 init_vars
 mv_install_pkg
 conf_file
