79685a038e0b2eb64f406a4caea9cec41e416481
root <root@host12.com>
2017-12-20 13:39:59 +0800

    basic

--- kafka_to_es/install.sh
+++ kafka_to_es/install.sh
@@ -1,18 +1,13 @@
-#!bin/bash
+#!/bin/bash
 
 function log() {
 	if [ -z $2 ]
 	then
-		echo $(date "+%G-%m-%d %H:%M:%S")": INF   detail: $1" >> $INSTALL_LOG
+		echo "$(date '+%G-%m-%d %H:%M:%S'): INF   detail: $1"  >> $LOG
 	else
-		echo $(date "+%G-%m-%d %H:%M:%S")": CRT   detail: $1" >> $INSTALL_LOG
+		echo "$(date '+%G-%m-%d %H:%M:%S'): CRT   detail: $1" >> $LOG 
 		exit 1
 	fi
-}
-
-function log_file() {
-	INSTALL_LOG="$LOG_DIR/install-$(date "+%G-%m-%d_%H:%M:%S").log"
-	touch $INSTALL_LOG
 }
 
 function env_var() {
@@ -27,32 +22,35 @@ function env_var() {
 }
 
 function init_vars() {
-	APT_HOME= $(env_var "${APT_HOME}" "get env_var APT_HOME")
+	LOG="$(pwd)/install-$(date '+%G-%m-%d_%H:%M:%S').log"
+	APT_HOME=$(env_var "${APT_HOME}" "get env_var APT_HOME")
 	KAFKA_TO_ES_PKG="$APT_HOME/package/kafka_to_es"
 	CONF_FILE="$KAFKA_TO_ES_PKG/conf/conf.ini"
-	LOG_DIR="$KAFKA_TO_ES_PKG/log"
 }
 
 function mv_install_pkg() {
 	if [ "" != "$(ls $APT_HOME/package | sed -n '/kafka_to_es/p')" ]
 	then
 		rm $KAFKA_TO_ES_PKG -rf
-		log "rm es_pkg in dest dir"
+		log "rm kafka_to_es in dest dir"
 	fi
 	
 	mkdir $KAFKA_TO_ES_PKG
 	cp ./kafka_to_es.tgz $KAFKA_TO_ES_PKG -r
 	cd $KAFKA_TO_ES_PKG
 	tar xzvf kafka_to_es.tgz 1>/dev/null
+        rm kafka_to_es.tgz -f
 	
 	log "put kafka_to_es pkg in dest dir"
 }
 
 function conf_file() {
-	partitionSet=0,1,2,3
+	partitionSet=0
+        #kafka_brokers=$(apt_config_show kafka brokers)
+	kafka_brokers=192.168.1.103,192.168.1.102
 	
-	nodes=192.168.1.103,192.168.1.102
-	port=9200
+	es_nodes=192.168.1.103,192.168.1.102
+	es_port=9200
 	
 	echo " " > $CONF_FILE
 	
@@ -62,15 +60,14 @@ waf-alert = '$partitionSet' \
 ids-alert = '$partitionSet' \
 \
 [kafka] \
-brokers = '$kafka-brokers' \
+brokers = '$kafka_brokers' \
 start = \
 \
 [elasticsearch] \
-nodes = '$nodes' \
-port = '$port'' $CONF_FILE	
+nodes = '$es_nodes' \
+port = '$es_port'' $CONF_FILE	
 }
 
 init_vars
-log_file
 mv_install_pkg
 conf_file
